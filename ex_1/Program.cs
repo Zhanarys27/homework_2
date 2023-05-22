6c0d7a2c2488f1e4a1e4ff42c3c91acee5dc1102
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-05-22 20:12:01 +0600

    Задача решена

--- ex_1/Program.cs
+++ ex_1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.Clear();
+
+int num = Convert.ToInt32(Console.ReadLine());
+if (num/100>0)
+{
+    if(num/1000>0)
+    {
+        System.Console.WriteLine("данное число больше 3 значного числа");
+    }
+    else
+    {
+        int x1 = num%100;
+        int x2 = x1/10;
+        System.Console.WriteLine(x2);
+    }
+}
+
+else
+{
+    System.Console.WriteLine("данное число меньше трехзначного числа");
+}
