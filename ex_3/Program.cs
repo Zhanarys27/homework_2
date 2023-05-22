1722179700f4176c6ecde03e552337f8117b35b5
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-05-23 03:47:49 +0600

    add ex_3

---
+++ ex_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int num = Convert.ToInt32(Console.ReadLine());
+if(num==6 || num == 7)
+{
+    System.Console.WriteLine("yeap, it's a weekend!");
+}
+else if (num <= 0 || num >= 7)
+{
+    System.Console.WriteLine("error");    
+}
+else
+{
+    System.Console.WriteLine("no");
+}
