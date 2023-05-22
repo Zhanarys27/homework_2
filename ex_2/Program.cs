e9e22749f0cbe3a5478fa2929e0aef60457a4847
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-05-23 03:22:44 +0600

    решенная задача 2

---
+++ ex_2/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+int num = Convert.ToInt32(Console.ReadLine());
+int mag = 0;
+int num1 = num;
+if (num/100>0)
+{
+     while(num>0)
+    {
+        mag=mag+1;
+        num= num/10;
+    }
+    
+    
+    
+    int i = 0;
+    //int j =0;
+    if(num1>=1000)
+    {
+        while (i+1 < mag-1)
+        {
+            i++;
+            mag--;
+            num1/=10;
+        
+        }
+        System.Console.WriteLine($"третье число: {num1%10}");
+    }
+    else
+    {
+        num1%=10;
+        System.Console.WriteLine($"третье число: {num1}");
+    }
+    
+}
+
+else
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+
+
+
+
+
